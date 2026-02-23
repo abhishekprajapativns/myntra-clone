ef55dc6467641168591d63329f70642c2a7cfe0c
mito-coder <abhishek.prajapati.it@gmail.com>
2026-02-23 15:45:06 +0530

    added config/db.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
@@ -9,18 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Myntra Backend Running!");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected!");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Error:", err);
-  });
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
