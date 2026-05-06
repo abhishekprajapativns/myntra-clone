de6186d3031091d899b8ae2796a7d1add43f7597
mito-coder <abhishek.prajapati.it@gmail.com>
2026-05-06 21:51:45 +0530

    Setup: React Router with all pages

--- Myntra_React/src/App.jsx
+++ Myntra_React/src/App.jsx
@@ -1,1 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Bag from "./pages/Bag";
+import Wishlist from "./pages/Wishlist";
+import Auth from "./pages/Auth";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/bag" element={<Bag />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
