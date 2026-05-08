dadad767e243e3aafa400d88706e30c289a25be6
mito-coder <abhishek.prajapati.it@gmail.com>
2026-05-08 11:48:23 +0530

    Add: WishlistContext

--- Myntra_React/src/pages/Wishlist.jsx
+++ Myntra_React/src/pages/Wishlist.jsx
@@ -1,9 +1,38 @@
-function Wishlist() {
+import { createContext, useState, useContext } from "react";
+
+const WishlistContext = createContext();
+
+export function WishlistProvider({ children }) {
+  const [wishlist, setWishlist] = useState([]);
+
+  function addToWishlist(product) {
+    const exists = wishlist.find((item) => item.id === product.id);
+    if (exists) {
+      // Already exists -> Remove (Toggle)
+
+      setWishlist((prev) => prev.filter((item) => item.id !== product.id));
+    } else {
+      // New -> Add
+
+      setWishlist((prev) => [...prev, product]);
+    }
+  }
+
+  function removeFromWishlist(id) {
+    setWishlist((prev) => prev.filter((item) => item.id !== id));
+  }
+
+  const totalWishlist = wishlist.length;
+
   return (
-    <div>
-      <h1>Wishlist Page</h1>
-    </div>
+    <WishlistContext.Provider
+      value={{ wishlist, addToWishlist, removeFromWishlist, totalWishlist }}
+    >
+      {children}
+    </WishlistContext.Provider>
   );
 }
 
-export default Wishlist;
+export function useWishlist() {
+  return useContext(WishlistContext);
+}
