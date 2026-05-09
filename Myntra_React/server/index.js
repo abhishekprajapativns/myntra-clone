7b05381fe9572d02b66e08576f4e7a86dc119fdb
mito-coder <abhishek.prajapati.it@gmail.com>
2026-05-09 12:54:51 +0530

    Add: index.js server setup

--- Myntra_React/server/index.js
+++ Myntra_React/server/index.js
@@ -1,23 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+// To Connect Database
+connectDB();
 
 const app = express();
 
 // For reading JSON data
 app.use(express.json());
 
-// Frontend se requests accept karne ke liye
+// To accept requests from the frontend
 app.use(cors());
-
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("mongoDB Connected!"))
-  .catch((err) => console.log("Error:", err));
 
 const PORT = process.env.PORT || 5000;
 
