325a05c30d0bee706055af6e7b47bd2776b652c8
mito-coder <abhishek.prajapati.it@gmail.com>
2026-05-10 13:42:54 +0530

    Add: Auth controller signup

---
+++ Myntra_React/server/controllers/authController.js
@@ -0,0 +1,30 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const signup = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists! " });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Account create successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error!", error });
+  }
+};
+
+module.exports = { signup };
