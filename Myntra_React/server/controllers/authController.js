7679d7f2255f364ebade3f66754d2d5f42d84e82
mito-coder <abhishek.prajapati.it@gmail.com>
2026-05-11 20:06:42 +0530

    Add: Auth controller login

--- Myntra_React/server/controllers/authController.js
+++ Myntra_React/server/controllers/authController.js
@@ -27,4 +27,38 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = { signup };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({
+      message: "Login successful!",
+      token,
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error!", error });
+  }
+};
+
+module.exports = { signup, login };
