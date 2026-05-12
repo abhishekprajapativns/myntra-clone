b1eeb0dd6b2dccad6d7c3d9f91309019fb224bb3
mito-coder <abhishek.prajapati.it@gmail.com>
2026-05-12 13:52:40 +0530

    Add: Auth routes connected to server

--- Myntra_React/server/index.js
+++ Myntra_React/server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
@@ -15,6 +16,8 @@ app.use(express.json());
 
 // To accept requests from the frontend
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
