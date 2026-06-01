d06976493bcf714b9ff96cbab04806964016d1cd
mito-coder <abhishek.prajapati.it@gmail.com>
2026-06-01 15:11:06 +0530

    feat: fix product list

--- Myntra_React/src/pages/Home.jsx
+++ Myntra_React/src/pages/Home.jsx
@@ -12,15 +12,26 @@ const banners = [
 ];
 
 const products = [
-  { id: 1, name: "Men's T-Shirt", price: 499, image: "/products/1.png" },
-  { id: 2, name: "Shoes", price: 1999, image: "/products/3.png" },
-  { id: 3, name: "Jacket", price: 2599, image: "/products/4.png" },
-  { id: 4, name: "Shirt", price: 699, image: "/products/7.png" },
-  { id: 5, name: "T-Shirt", price: 999, image: "/products/8.png" },
-  { id: 6, name: "Shoes", price: 999, image: "/products/9.png" },
-  { id: 7, name: "Shoes", price: 999, image: "/products/10.png" },
-  { id: 8, name: "Sandal", price: 999, image: "/products/11.png" },
-  { id: 9, name: "Jeans", price: 999, image: "/products/12.png" },
+  { id: 1, name: "Sports Wear", price: 499, image: "/products/1.png" },
+  { id: 2, name: "Casual Wear", price: 899, image: "/products/2.png" },
+  { id: 3, name: "Sneakers", price: 1999, image: "/products/3.png" },
+  { id: 4, name: "Jacket", price: 2599, image: "/products/4.png" },
+  { id: 5, name: "Shirt", price: 799, image: "/products/5.png" },
+  { id: 6, name: "Women Sandals", price: 1299, image: "/products/6.png" },
+  { id: 8, name: "T-Shirt", price: 999, image: "/products/8.png" },
+  { id: 9, name: "Casual Shoes", price: 1499, image: "/products/9.png" },
+  { id: 10, name: "Shoes", price: 2299, image: "/products/10.png" },
+  { id: 11, name: "Women Sandals", price: 799, image: "/products/11.png" },
+  { id: 12, name: "Jeans", price: 1299, image: "/products/12.png" },
+  { id: 13, name: "Watches", price: 2499, image: "/products/18.webp" },
+  { id: 14, name: "Formal Shoes", price: 599, image: "/products/19.webp" },
+  { id: 15, name: "Flips", price: 2799, image: "/products/20.webp" },
+  { id: 16, name: "Derby Shoes", price: 2199, image: "/products/21.webp" },
+  { id: 17, name: "Men Sandals", price: 3499, image: "/products/22.webp" },
+  { id: 18, name: "Loafers", price: 1799, image: "/products/24.webp" },
+  { id: 19, name: "Ankle Boots", price: 2999, image: "/products/25.webp" },
+  { id: 20, name: "Watches", price: 2499, image: "/products/31.webp" },
+  { id: 21, name: "Canvas Shoes", price: 1299, image: "/products/33.webp" },
 ];
 
 function Home() {
